7c3187fe38dda656b16438113a049ba5a5f4585a
Ash Goren <ash@mortalwombat.net>
2026-03-19 22:48:58 -0700

    split vendor libraries into manual chunks

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,5 +9,20 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-router':   ['react-router'],
+          'vendor-mui':      ['@mui/material', '@mui/icons-material', '@mui/system', '@emotion/react', '@emotion/styled', '@mui/x-date-pickers', 'date-fns'],
+          'vendor-mrt':      ['material-react-table', '@tanstack/react-table'],
+          'vendor-query':    ['@tanstack/react-query'],
+          'vendor-supabase': ['@supabase/supabase-js'],
+          'vendor-dnd':      ['@dnd-kit/react', '@dnd-kit/helpers'],
+          'vendor-md-editor':  ['@uiw/react-md-editor'],
+        }
+      }
+    }
   }
 })
