d8502a64354e0d0925d97035956e7bf3ac1092ed
Ash Goren <ash@mortalwombat.net>
2026-04-06 23:26:25 -0700

    init placeholder ios app

---
+++ ios/CallerStudio/CallerStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallerStudioApp.swift
+//  CallerStudio
+//
+//  Created by Ash on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CallerStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/CallerStudio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CallerStudio
+//
+//  Created by Ash on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
